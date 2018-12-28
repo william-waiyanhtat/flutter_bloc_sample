5e6103c888fa09a3fd369d7dbf0ade112c109699
Vipin Vijayan <vipinvijayan@Vipins-MacBook-Pro.local>
2018-12-28 08:29:49 -0500

    Flutterdebugging

--- FlutterTutorialProjects/flutter_demo1/lib/main.dart
+++ FlutterTutorialProjects/flutter_demo1/lib/main.dart
@@ -1,18 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_demo1/file_operations/file_operations.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_demo1/debug/debug_app.dart';
+import 'package:flutter/scheduler.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  // You can also debug a layout problem visually, by setting debugPaintSizeEnabled to true.
+  // debugPaintSizeEnabled = true;
+  // Debugging animations
+  // timeDilation = 50.0;
+  // debugPaintPointersEnabled = true;
+  debugPrintBeginFrameBanner = true;
+  debugPaintLayerBordersEnabled = true;
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      //showPerformanceOverlay:true,
+      //debugShowMaterialGrid: true,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: FileOperationsScreen(),
+      home: DebugAppScene(),
     );
   }
 }
