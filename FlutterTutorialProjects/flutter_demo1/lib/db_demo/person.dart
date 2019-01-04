b4cf7a4c2b63492f83c3417bec73f507f5ed14d1
Vipin Vijayan <vipinvijayan@Vipins-MacBook-Pro.local>
2019-01-04 08:25:10 -0500

    Android Store Image SQLite and Flutter Forms

---
+++ FlutterTutorialProjects/flutter_demo1/lib/db_demo/person.dart
@@ -0,0 +1,7 @@
+class Person {
+  String name;
+  String age;
+  String mobileNo;
+
+  Person(this.name, this.age, this.mobileNo);
+}
--- FlutterTutorialProjects/flutter_demo1/lib/main.dart
+++ FlutterTutorialProjects/flutter_demo1/lib/main.dart
@@ -4,6 +4,8 @@ import 'package:flutter_demo1/debug/debug_app.dart';
 import 'package:flutter/scheduler.dart';
 import 'widgets/switch_demo.dart';
 import 'package:flutter_demo1/custom_widget/custom_widget_demo.dart';
+import 'db_demo/db_page.dart';
+import 'forms/form.dart';
 
 void main() {
   // You can also debug a layout problem visually, by setting debugPaintSizeEnabled to true.
@@ -27,7 +29,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: CustomWidgetDemo(),
+      home: FormScreen(),
     );
   }
 }
