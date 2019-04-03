87d6bd70c4c5aec1a3ff5972956b88f991e1438a
Vipin Vijayan <vipinvijayan@Vipins-MacBook-Pro-2.local>
2019-04-02 20:16:58 -0400

    Tip3 Added

---
+++ FlutterTutorialProjects/flutter_demo1/lib/tips/tip3.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Tip3 extends StatefulWidget {
+  Tip3() : super();
+
+  final String title = "Tip3 Demo";
+
+  @override
+  Tip3State createState() => Tip3State();
+}
+
+class Tip3State extends State<Tip3> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(),
+    );
+  }
+}
