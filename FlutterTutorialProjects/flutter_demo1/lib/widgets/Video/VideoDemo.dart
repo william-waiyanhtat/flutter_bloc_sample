05d96cb87f57b5906e6b78341f08fc3867b8255d
Vipin Vijayan <vipinvijayan@Vipins-MacBook-Pro-2.local>
2019-05-25 23:21:03 -0400

    ...

--- FlutterTutorialProjects/flutter_demo1/lib/widgets/Video/VideoDemo.dart
+++ FlutterTutorialProjects/flutter_demo1/lib/widgets/Video/VideoDemo.dart
@@ -11,13 +11,61 @@ class VideoDemo extends StatefulWidget {
 }
 
 class VideoDemoState extends State<VideoDemo> {
+  //
+  VideoPlayerController _controller;
+  Future<void> _initializeVideoPlayerFuture;
+
+  @override
+  void initState() {
+    _controller = VideoPlayerController.network(
+        "https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4");
+    //_controller = VideoPlayerController.asset("videos/sample_video.mp4");
+    _initializeVideoPlayerFuture = _controller.initialize();
+    _controller.setLooping(true);
+    _controller.setVolume(1.0);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Video Demo"),
       ),
-      body: Container(),
+      body: FutureBuilder(
+        future: _initializeVideoPlayerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return AspectRatio(
+              aspectRatio: _controller.value.aspectRatio,
+              child: VideoPlayer(_controller),
+            );
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            if (_controller.value.isPlaying) {
+              _controller.pause();
+            } else {
+              _controller.play();
+            }
+          });
+        },
+        child:
+            Icon(_controller.value.isPlaying ? Icons.pause : Icons.play_arrow),
+      ),
     );
   }
 }
