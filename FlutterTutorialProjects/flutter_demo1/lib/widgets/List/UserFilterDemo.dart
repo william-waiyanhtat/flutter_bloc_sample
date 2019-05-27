de42372a4acbac5f919cdb87f4c33ba0b6d30a83
Vipin Vijayan <vipinvijayan@Vipins-MacBook-Pro-2.local>
2019-05-26 21:20:40 -0400

    ...

--- FlutterTutorialProjects/flutter_demo1/lib/widgets/List/UserFilterDemo.dart
+++ FlutterTutorialProjects/flutter_demo1/lib/widgets/List/UserFilterDemo.dart
@@ -14,7 +14,7 @@ class UserFilterDemoState extends State<UserFilterDemo> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Users List'),
+        title: Text(widget.title),
       ),
       body: Container(),
     );
