8520c795336aa26b3b414de3652a3e92d33c594e
Vipin Vijayan <vipinvijayan@Vipins-MacBook-Pro-2.local>
2019-06-15 15:20:38 -0400

    firebase upload and download

--- FlutterTutorialProjects/flutter_demo1/lib/UploadFile/UploadMultipleImage.dart
+++ FlutterTutorialProjects/flutter_demo1/lib/UploadFile/UploadMultipleImage.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
 import 'dart:io';
-import 'dart:convert';
-import 'package:http/http.dart' as http;
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:file_picker/file_picker.dart';
 
 import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
 
 class UploadMultipleImageDemo extends StatefulWidget {
   UploadMultipleImageDemo() : super();
 
-  final String title = "Upload Image Demo";
+  final String title = 'Firebase File Upload';
 
   @override
   UploadMultipleImageDemoState createState() => UploadMultipleImageDemoState();
@@ -18,186 +17,258 @@ class UploadMultipleImageDemo extends StatefulWidget {
 
 class UploadMultipleImageDemoState extends State<UploadMultipleImageDemo> {
   //
-
-  String _fileName;
+  GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
   String _path;
   Map<String, String> _paths;
   String _extension;
   bool _multiPick = false;
-  bool _hasValidMime = false;
-  FileType _pickingType;
-  TextEditingController _controller = new TextEditingController();
-
-  @override
-  void initState() {
-    super.initState();
-    _controller.addListener(() => _extension = _controller.text);
-  }
+  FileType _pickType;
+  List<StorageUploadTask> _tasks = <StorageUploadTask>[];
 
   void _openFileExplorer() async {
-    if (_pickingType != FileType.CUSTOM || _hasValidMime) {
-      try {
-        if (_multiPick) {
-          _path = null;
-          _paths = await FilePicker.getMultiFilePath(
-              type: _pickingType, fileExtension: _extension);
-        } else {
-          _paths = null;
-          _path = await FilePicker.getFilePath(
-              type: _pickingType, fileExtension: _extension);
-          final StorageReference storageRef =
-              FirebaseStorage.instance.ref().child(_path);
-          final StorageUploadTask uploadTask = storageRef.putFile(
-            File(_path),
-            StorageMetadata(
-              contentType: 'image/jpeg',
-            ),
-          );
-        }
-      } on PlatformException catch (e) {
-        print("Unsupported operation" + e.toString());
+    try {
+      print(_multiPick);
+      if (_multiPick) {
+        _path = null;
+        _paths = await FilePicker.getMultiFilePath(
+            type: _pickType, fileExtension: _extension);
+      } else {
+        _path = null;
+        _path = await FilePicker.getFilePath(
+            type: _pickType, fileExtension: _extension);
       }
-      if (!mounted) return;
-
-      setState(() {
-        _fileName = _path != null
-            ? _path.split('/').last
-            : _paths != null ? _paths.keys.toString() : '...';
-      });
+      uploadToFirebase();
+    } on PlatformException catch (e) {
+      print("Unsupported operation" + e.toString());
     }
+    if (!mounted) return;
+  }
+
+  uploadToFirebase() {
+    if (_multiPick) {
+      _paths.forEach((fileName, filePath) => {upload(fileName, filePath)});
+    } else {
+      String fileName = _path.split('/').last;
+      String filePath = _path;
+      upload(fileName, filePath);
+    }
+  }
+
+  upload(fileName, filePath) {
+    _extension = fileName.toString().split('.').last;
+    StorageReference storageRef =
+        FirebaseStorage.instance.ref().child(fileName);
+    final StorageUploadTask uploadTask = storageRef.putFile(
+      File(filePath),
+      StorageMetadata(
+        contentType: '$_pickType/$_extension',
+      ),
+    );
+    setState(() {
+      _tasks.add(uploadTask);
+    });
+  }
+
+  dropDown() {
+    return DropdownButton(
+      hint: new Text('Select'),
+      value: _pickType,
+      items: <DropdownMenuItem>[
+        new DropdownMenuItem(
+          child: new Text('Audio'),
+          value: FileType.AUDIO,
+        ),
+        new DropdownMenuItem(
+          child: new Text('Image'),
+          value: FileType.IMAGE,
+        ),
+        new DropdownMenuItem(
+          child: new Text('Video'),
+          value: FileType.VIDEO,
+        ),
+        new DropdownMenuItem(
+          child: new Text('Any'),
+          value: FileType.ANY,
+        ),
+      ],
+      onChanged: (value) => setState(
+            () {
+              _pickType = value;
+            },
+          ),
+    );
+  }
+
+  String _bytesTransferred(StorageTaskSnapshot snapshot) {
+    return '${snapshot.bytesTransferred}/${snapshot.totalByteCount}';
   }
 
   @override
   Widget build(BuildContext context) {
+    final List<Widget> children = <Widget>[];
+    _tasks.forEach((StorageUploadTask task) {
+      final Widget tile = UploadTaskListTile(
+        task: task,
+        onDismissed: () => setState(() => _tasks.remove(task)),
+        onDownload: () => _downloadFile(task.lastSnapshot.ref),
+      );
+      children.add(tile);
+    });
+
     return new MaterialApp(
       home: new Scaffold(
+        key: _scaffoldKey,
         appBar: new AppBar(
-          title: const Text('File Picker example app'),
+          title: Text(widget.title),
         ),
-        body: new Center(
-            child: new Padding(
-          padding: const EdgeInsets.only(left: 10.0, right: 10.0),
-          child: new SingleChildScrollView(
-            child: new Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                new Padding(
-                  padding: const EdgeInsets.only(top: 20.0),
-                  child: new DropdownButton(
-                      hint: new Text('LOAD PATH FROM'),
-                      value: _pickingType,
-                      items: <DropdownMenuItem>[
-                        new DropdownMenuItem(
-                          child: new Text('FROM AUDIO'),
-                          value: FileType.AUDIO,
-                        ),
-                        new DropdownMenuItem(
-                          child: new Text('FROM IMAGE'),
-                          value: FileType.IMAGE,
-                        ),
-                        new DropdownMenuItem(
-                          child: new Text('FROM VIDEO'),
-                          value: FileType.VIDEO,
-                        ),
-                        new DropdownMenuItem(
-                          child: new Text('FROM ANY'),
-                          value: FileType.ANY,
-                        ),
-                        new DropdownMenuItem(
-                          child: new Text('CUSTOM FORMAT'),
-                          value: FileType.CUSTOM,
-                        ),
-                      ],
-                      onChanged: (value) => setState(() {
-                            _pickingType = value;
-                            if (_pickingType != FileType.CUSTOM) {
-                              _controller.text = _extension = '';
-                            }
-                          })),
+        body: new Container(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              dropDown(),
+              SwitchListTile.adaptive(
+                title: Text('Pick multiple files', textAlign: TextAlign.left),
+                onChanged: (bool value) => setState(() => _multiPick = value),
+                value: _multiPick,
+              ),
+              OutlineButton(
+                onPressed: () => _openFileExplorer(),
+                child: new Text("Open file picker"),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Flexible(
+                child: ListView(
+                  children: children,
                 ),
-                ConstrainedBox(
-                  constraints: BoxConstraints.tightFor(width: 100.0),
-                  child: _pickingType == FileType.CUSTOM
-                      ? new TextFormField(
-                          maxLength: 15,
-                          autovalidate: true,
-                          controller: _controller,
-                          decoration:
-                              InputDecoration(labelText: 'File extension'),
-                          keyboardType: TextInputType.text,
-                          textCapitalization: TextCapitalization.none,
-                          validator: (value) {
-                            RegExp reg = new RegExp(r'[^a-zA-Z0-9]');
-                            if (reg.hasMatch(value)) {
-                              _hasValidMime = false;
-                              return 'Invalid format';
-                            }
-                            _hasValidMime = true;
-                          },
-                        )
-                      : new Container(),
-                ),
-                new ConstrainedBox(
-                  constraints: BoxConstraints.tightFor(width: 200.0),
-                  child: new SwitchListTile.adaptive(
-                    title: new Text('Pick multiple files',
-                        textAlign: TextAlign.right),
-                    onChanged: (bool value) =>
-                        setState(() => _multiPick = value),
-                    value: _multiPick,
-                  ),
-                ),
-                new Padding(
-                  padding: const EdgeInsets.only(top: 50.0, bottom: 20.0),
-                  child: new RaisedButton(
-                    onPressed: () => _openFileExplorer(),
-                    child: new Text("Open file picker"),
-                  ),
-                ),
-                new Builder(
-                  builder: (BuildContext context) => new Container(
-                        padding: const EdgeInsets.only(bottom: 30.0),
-                        height: MediaQuery.of(context).size.height * 0.50,
-                        child: new Scrollbar(
-                          child: _path != null || _paths != null
-                              ? new ListView.separated(
-                                  itemCount: _paths != null && _paths.isNotEmpty
-                                      ? _paths.length
-                                      : 1,
-                                  itemBuilder:
-                                      (BuildContext context, int index) {
-                                    final bool isMultiPath =
-                                        _paths != null && _paths.isNotEmpty;
-                                    final String name = 'File $index: ' +
-                                        (isMultiPath
-                                            ? _paths.keys.toList()[index]
-                                            : _fileName ?? '...');
-                                    final path = isMultiPath
-                                        ? _paths.values
-                                            .toList()[index]
-                                            .toString()
-                                        : _path;
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 
-                                    return new ListTile(
-                                      title: new Text(
-                                        name,
-                                      ),
-                                      subtitle: new Text(path),
-                                    );
-                                  },
-                                  separatorBuilder:
-                                      (BuildContext context, int index) =>
-                                          new Divider(),
-                                )
-                              : new Container(),
-                        ),
-                      ),
+  Future<void> _downloadFile(StorageReference ref) async {
+    final String url = await ref.getDownloadURL();
+    final http.Response downloadData = await http.get(url);
+    final Directory systemTempDir = Directory.systemTemp;
+    final File tempFile = File('${systemTempDir.path}/tmp.jpg');
+    if (tempFile.existsSync()) {
+      await tempFile.delete();
+    }
+    await tempFile.create();
+    final StorageFileDownloadTask task = ref.writeToFile(tempFile);
+    final int byteCount = (await task.future).totalByteCount;
+    var bodyBytes = downloadData.bodyBytes;
+    final String name = await ref.getName();
+    final String path = await ref.getPath();
+    print(
+      'Success!\nDownloaded $name \nUrl: $url'
+      '\npath: $path \nBytes Count :: $byteCount',
+    );
+    _scaffoldKey.currentState.showSnackBar(
+      SnackBar(
+        backgroundColor: Colors.white,
+        content: Image.memory(
+          bodyBytes,
+          fit: BoxFit.fill,
+        ),
+      ),
+    );
+  }
+}
+
+class UploadTaskListTile extends StatelessWidget {
+  const UploadTaskListTile(
+      {Key key, this.task, this.onDismissed, this.onDownload})
+      : super(key: key);
+
+  final StorageUploadTask task;
+  final VoidCallback onDismissed;
+  final VoidCallback onDownload;
+
+  String get status {
+    String result;
+    if (task.isComplete) {
+      if (task.isSuccessful) {
+        result = 'Complete';
+      } else if (task.isCanceled) {
+        result = 'Canceled';
+      } else {
+        result = 'Failed ERROR: ${task.lastSnapshot.error}';
+      }
+    } else if (task.isInProgress) {
+      result = 'Uploading';
+    } else if (task.isPaused) {
+      result = 'Paused';
+    }
+    return result;
+  }
+
+  String _bytesTransferred(StorageTaskSnapshot snapshot) {
+    return '${snapshot.bytesTransferred}/${snapshot.totalByteCount}';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<StorageTaskEvent>(
+      stream: task.events,
+      builder: (BuildContext context,
+          AsyncSnapshot<StorageTaskEvent> asyncSnapshot) {
+        Widget subtitle;
+        if (asyncSnapshot.hasData) {
+          final StorageTaskEvent event = asyncSnapshot.data;
+          final StorageTaskSnapshot snapshot = event.snapshot;
+          subtitle = Text('$status: ${_bytesTransferred(snapshot)} bytes sent');
+        } else {
+          subtitle = const Text('Starting...');
+        }
+        return Dismissible(
+          key: Key(task.hashCode.toString()),
+          onDismissed: (_) => onDismissed(),
+          child: ListTile(
+            title: Text('Upload Task #${task.hashCode}'),
+            subtitle: subtitle,
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Offstage(
+                  offstage: !task.isInProgress,
+                  child: IconButton(
+                    icon: const Icon(Icons.pause),
+                    onPressed: () => task.pause(),
+                  ),
+                ),
+                Offstage(
+                  offstage: !task.isPaused,
+                  child: IconButton(
+                    icon: const Icon(Icons.file_upload),
+                    onPressed: () => task.resume(),
+                  ),
+                ),
+                Offstage(
+                  offstage: task.isComplete,
+                  child: IconButton(
+                    icon: const Icon(Icons.cancel),
+                    onPressed: () => task.cancel(),
+                  ),
+                ),
+                Offstage(
+                  offstage: !(task.isComplete && task.isSuccessful),
+                  child: IconButton(
+                    icon: const Icon(Icons.file_download),
+                    onPressed: onDownload,
+                  ),
                 ),
               ],
             ),
           ),
-        )),
-      ),
+        );
+      },
     );
   }
 }
