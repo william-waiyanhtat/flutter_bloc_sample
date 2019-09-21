ac73c4a9e653d1ebbcc15c11881d5384b4059b3d
Vipin <vipinvijayan@Vipins-MacBook-Pro-2.local>
2019-09-20 23:50:08 -0400

    ...

--- FlutterTutorialProjects/flutter_demo1/lib/widgets/MySQL_DataTable/Services.dart
+++ FlutterTutorialProjects/flutter_demo1/lib/widgets/MySQL_DataTable/Services.dart
@@ -3,12 +3,19 @@ import 'package:http/http.dart' as http;
 import 'Employees.dart';
 
 class Services {
-  static const String url = "http://localhost/Employees/get_employees.php";
+  static const ROOT = 'http://localhost/Employees/employee_actions.php';
+  static const String _GET_ACTION = 'GET_ALL';
+  static const String _CREATE_ACTION = 'CREATE_TABLE';
+  static const String _ADD_EMP_ACTION = 'ADD_EMP';
+  static const String _UPDATE_EMP_ACTION = 'UPDATE_EMP';
+  static const String _DELETE_EMP_ACTIOn = 'DELETE_EMP';
 
   static Future<List<Employee>> getEmployees() async {
     try {
-      final response = await http.get(url);
-      print("Response:: ${response.body}");
+      var map = new Map<String, dynamic>();
+      map["action"] = _GET_ACTION;
+      final response = await http.post(ROOT, body: map);
+      print("getEmployees >> Response:: ${response.body}");
       if (response.statusCode == 200) {
         List<Employee> list = parsePhotos(response.body);
         return list;
@@ -16,8 +23,6 @@ class Services {
         throw Exception("Error");
       }
     } catch (e) {
-      //throw Exception(e.toString());
-      print("Error ${e.toString()}");
       return List<Employee>();
     }
   }
@@ -26,4 +31,34 @@ class Services {
     final parsed = json.decode(responseBody).cast<Map<String, dynamic>>();
     return parsed.map<Employee>((json) => Employee.fromJson(json)).toList();
   }
+
+  // static Future<bool> createTable() async {
+  //   try {
+  //     final response = await http.get(_create_table_url);
+  //     print("createTable >> Response:: ${response.body}");
+  //     return 'success' == response.body;
+  //   } catch (e) {
+  //     return false;
+  //   }
+  // }
+
+  // static Future<bool> addEmployee(String firstName, String lastName) async {
+  //   try {
+  //     final response = await http.get(_create_table_url);
+  //     print("addEmployee >> Response:: ${response.body}");
+  //     return 'success' == response.body;
+  //   } catch (e) {
+  //     return false;
+  //   }
+  // }
+
+  // static Future<bool> deleteEmployee(String empId) async {
+  //   try {
+  //     final response = await http.get(_delete_employee_url);
+  //     print("deleteEmployee >> Response:: ${response.body}");
+  //     return 'success' == response.body;
+  //   } catch (e) {
+  //     return false;
+  //   }
+  // }
 }
