01c75237783dd3d36295e4acb6ec6baab3d7c765
Vipin <vipinvijayan@Vipins-MacBook-Pro-2.local>
2019-10-26 15:26:11 -0400

    ...

--- FlutterTutorialProjects/flutter_demo1/lib/widgets/Charts/ChartsDemo.dart
+++ FlutterTutorialProjects/flutter_demo1/lib/widgets/Charts/ChartsDemo.dart
@@ -25,8 +25,9 @@ class ChartsDemoState extends State<ChartsDemo> {
     animate = true;
   }
 
-  /// Create random data.
-  static List<charts.Series<Sales, String>> _createStackedRandomData() {
+  // Create random data.
+  static List<charts.Series<Sales, String>> _createRandomData() {
+    //
     final random = new Random();
 
     final desktopSalesData = [
@@ -52,63 +53,41 @@ class ChartsDemoState extends State<ChartsDemo> {
 
     return [
       new charts.Series<Sales, String>(
-        id: 'Desktop',
-        domainFn: (Sales sales, _) => sales.year,
-        measureFn: (Sales sales, _) => sales.sales,
-        data: desktopSalesData,
-      ),
-      new charts.Series<Sales, String>(
-        id: 'Tablet',
-        domainFn: (Sales sales, _) => sales.year,
-        measureFn: (Sales sales, _) => sales.sales,
-        data: tableSalesData,
-      ),
-      new charts.Series<Sales, String>(
-        id: 'Mobile',
-        domainFn: (Sales sales, _) => sales.year,
-        measureFn: (Sales sales, _) => sales.sales,
-        data: mobileSalesData,
-      ),
-    ];
-  }
-
-  // Create random data.
-  static List<charts.Series<Sales, String>> _createRandomData() {
-    //
-    final random = new Random();
-
-    final data = [
-      new Sales('2014', random.nextInt(100)),
-      new Sales('2015', random.nextInt(100)),
-      new Sales('2016', random.nextInt(100)),
-      new Sales('2017', random.nextInt(100)),
-      new Sales('2018', random.nextInt(100)),
-    ];
-
-    return [
-      new charts.Series<Sales, String>(
         id: 'Sales',
-        //colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
         domainFn: (Sales sales, _) => sales.year,
         measureFn: (Sales sales, _) => sales.sales,
-        data: data,
+        labelAccessorFn: (Sales sales, _) =>
+            '${sales.year}: \$${sales.sales.toString()}',
+        data: desktopSalesData,
         fillColorFn: (Sales sales, _) {
           return (sales.year == '2015')
               ? charts.MaterialPalette.red.shadeDefault
               : charts.MaterialPalette.green.shadeDefault;
         },
-        insideLabelStyleAccessorFn: (Sales sales, _) {
-          final color = (sales.year == '2014')
-              ? charts.MaterialPalette.red.shadeDefault
-              : charts.MaterialPalette.yellow.shadeDefault.darker;
-          return new charts.TextStyleSpec(color: color);
+      ),
+      new charts.Series<Sales, String>(
+        id: 'Sales',
+        domainFn: (Sales sales, _) => sales.year,
+        measureFn: (Sales sales, _) => sales.sales,
+        data: tableSalesData,
+        fillColorFn: (Sales sales, _) {
+          return (sales.year == '2015')
+              ? charts.MaterialPalette.yellow.shadeDefault
+              : charts.MaterialPalette.green.shadeDefault;
         },
-        // outsideLabelStyleAccessorFn: (Sales sales, _) {
-        //   final color = (sales.year == '2015')
-        //       ? charts.MaterialPalette.red.shadeDefault
-        //       : charts.MaterialPalette.yellow.shadeDefault.darker;
-        //   return new charts.TextStyleSpec(color: color);
-        // },
+      ),
+      new charts.Series<Sales, String>(
+        id: 'Sales',
+        domainFn: (Sales sales, _) => sales.year,
+        measureFn: (Sales sales, _) => sales.sales,
+        labelAccessorFn: (Sales sales, _) =>
+            '${sales.year}: \$${sales.sales.toString()}',
+        data: mobileSalesData,
+        fillColorFn: (Sales sales, _) {
+          return (sales.year == '2015')
+              ? charts.MaterialPalette.blue.shadeDefault
+              : charts.MaterialPalette.green.shadeDefault;
+        },
       )
     ];
   }
@@ -130,16 +109,14 @@ class ChartsDemoState extends State<ChartsDemo> {
     return charts.BarChart(
       seriesList,
       animate: animate,
-      //barGroupingType: charts.BarGroupingType.stacked,
+      barGroupingType: charts.BarGroupingType.grouped,
       // defaultRenderer: new charts.BarRendererConfig(
       //   cornerStrategy: const charts.ConstCornerStrategy(30),
       // ),
       defaultRenderer: new charts.BarRendererConfig(
-          groupingType: charts.BarGroupingType.stacked, strokeWidthPx: 2.0),
-      // defaultRenderer: new charts.BarRendererConfig(
-      //     groupingType: charts.BarGroupingType.grouped, strokeWidthPx: 2.0),
+          groupingType: charts.BarGroupingType.grouped, strokeWidthPx: 2.0),
       vertical: false,
-      barRendererDecorator: new charts.BarLabelDecorator<String>(),
+      //barRendererDecorator: new charts.BarLabelDecorator<String>(),
       // Hide domain axis.
       // domainAxis: new charts.OrdinalAxisSpec(
       //   renderSpec: new charts.NoneRenderSpec(),
