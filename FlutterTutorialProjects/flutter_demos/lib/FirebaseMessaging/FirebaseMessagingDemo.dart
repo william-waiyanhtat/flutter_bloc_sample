b9db42bdf187208ea417e3149295514aee595d68
Vipin Vijayan <vipin.vijayannair@ust-global.com>
2020-01-10 16:16:38 -0500

    ...

--- FlutterTutorialProjects/flutter_demos/lib/FirebaseMessaging/FirebaseMessagingDemo.dart
+++ FlutterTutorialProjects/flutter_demos/lib/FirebaseMessaging/FirebaseMessagingDemo.dart
@@ -14,30 +14,35 @@ class FirebaseMessagingDemo extends StatefulWidget {
 class _FirebaseMessagingDemoState extends State<FirebaseMessagingDemo> {
   //
   final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+  List<Message> messagesList;
 
   @override
   void initState() {
     super.initState();
+    messagesList = List<Message>();
+    _getToken();
+    _configureFirebaseListeners();
+  }
+
+  _getToken() {
     _firebaseMessaging.getToken().then((token) {
       print("Device Token: $token");
     });
+  }
+
+  _configureFirebaseListeners() {
     _firebaseMessaging.configure(
       onMessage: (Map<String, dynamic> message) async {
         print('onMessage: $message');
-        final notification = message['notification'];
-        final data = message['data'];
-        print(
-            "Title: ${notification['title']}, body: ${notification['body']} , key1: body: ${data['key1']}");
+        _setMessage(message);
       },
       onLaunch: (Map<String, dynamic> message) async {
         print('onLaunch: $message');
-        final notification = message['notification'];
-        print("Title: ${notification['title']}, body: ${notification['body']}");
+        _setMessage(message);
       },
       onResume: (Map<String, dynamic> message) async {
         print('onResume: $message');
-        final notification = message['notification'];
-        print("Title: ${notification['title']}, body: ${notification['body']}");
+        _setMessage(message);
       },
     );
     _firebaseMessaging.requestNotificationPermissions(
@@ -45,13 +50,69 @@ class _FirebaseMessagingDemoState extends State<FirebaseMessagingDemo> {
     );
   }
 
+  _setMessage(Map<String, dynamic> message) {
+    final notification = message['notification'];
+    final data = message['data'];
+    final String title = notification['title'];
+    final String body = notification['body'];
+    String dataKey = data['key1'];
+    print("Title: $title, body: $body, Key1: $dataKey");
+    setState(() {
+      Message msg = Message(title, body, dataKey);
+      messagesList.add(msg);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    print('here');
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Container(),
+      body: ListView.builder(
+        itemCount: _getMessageCount(),
+        itemBuilder: (BuildContext context, int index) {
+          return row(index);
+        },
+      ),
     );
+  }
+
+  _getMessageCount() {
+    print("len ${messagesList.length}");
+    return null == messagesList ? 0 : messagesList.length;
+  }
+
+  Widget row(int index) {
+    return Card(
+      child: Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              messagesList[index].dataKey1,
+              style: TextStyle(
+                fontSize: 16.0,
+                color: Colors.black,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Message {
+  String title;
+  String body;
+  String dataKey1;
+  Message(title, body, dataKey1) {
+    this.title = title;
+    this.body = body;
+    this.dataKey1 = dataKey1;
   }
 }
