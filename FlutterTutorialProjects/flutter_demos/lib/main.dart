70cc8601c6d3eb183a8f0d81718d6fe2072ee880
Vipin <vipinvijayan23@gmail.com>
2020-01-09 23:35:10 -0500

    Cloud functions added

--- FlutterTutorialProjects/flutter_demos/lib/main.dart
+++ FlutterTutorialProjects/flutter_demos/lib/main.dart
@@ -1,6 +1,4 @@
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'widgets/CustomWidgets/ExtendedDemo.dart';
 import 'FirebaseMessaging/FirebaseMessagingDemo.dart';
 
 void main() {
