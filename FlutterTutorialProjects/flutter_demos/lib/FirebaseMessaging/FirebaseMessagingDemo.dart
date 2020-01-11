b756e01a99c7ba429f150e62988f280d380b2b71
Vipin <vipinvijayan23@gmail.com>
2020-01-10 22:47:22 -0500

    ...

--- FlutterTutorialProjects/flutter_demos/lib/FirebaseMessaging/FirebaseMessagingDemo.dart
+++ FlutterTutorialProjects/flutter_demos/lib/FirebaseMessaging/FirebaseMessagingDemo.dart
@@ -55,52 +55,36 @@ class _FirebaseMessagingDemoState extends State<FirebaseMessagingDemo> {
     final data = message['data'];
     final String title = notification['title'];
     final String body = notification['body'];
-    String dataKey = data['key1'];
-    print("Title: $title, body: $body, Key1: $dataKey");
+    String mMessage = data['message'];
+    print("Title: $title, body: $body, message: $mMessage");
     setState(() {
-      Message msg = Message(title, body, dataKey);
+      Message msg = Message(title, body, mMessage);
       messagesList.add(msg);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    print('here');
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
       body: ListView.builder(
-        itemCount: _getMessageCount(),
+        itemCount: null == messagesList ? 0 : messagesList.length,
         itemBuilder: (BuildContext context, int index) {
-          return row(index);
-        },
-      ),
-    );
-  }
-
-  _getMessageCount() {
-    print("len ${messagesList.length}");
-    return null == messagesList ? 0 : messagesList.length;
-  }
-
-  Widget row(int index) {
-    return Card(
-      child: Padding(
-        padding: EdgeInsets.all(10.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              messagesList[index].dataKey1,
-              style: TextStyle(
-                fontSize: 16.0,
-                color: Colors.black,
+          return Card(
+            child: Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Text(
+                messagesList[index].message,
+                style: TextStyle(
+                  fontSize: 16.0,
+                  color: Colors.black,
+                ),
               ),
-            )
-          ],
-        ),
+            ),
+          );
+        },
       ),
     );
   }
@@ -109,10 +93,10 @@ class _FirebaseMessagingDemoState extends State<FirebaseMessagingDemo> {
 class Message {
   String title;
   String body;
-  String dataKey1;
-  Message(title, body, dataKey1) {
+  String message;
+  Message(title, body, message) {
     this.title = title;
     this.body = body;
-    this.dataKey1 = dataKey1;
+    this.message = message;
   }
 }
