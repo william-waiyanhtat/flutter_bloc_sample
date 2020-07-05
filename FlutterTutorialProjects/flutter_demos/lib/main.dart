288046616cb0c3374de61ac87eba42417cedf6e6
Vipin <vipinvijayan23@gmail.com>
2020-07-05 10:02:23 -0400

    Removed Unwanted filess

--- FlutterTutorialProjects/flutter_demos/lib/main.dart
+++ FlutterTutorialProjects/flutter_demos/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'FirebaseMessaging/FirebaseMessagingDemo.dart';
+import 'widgets/MySQL_DataTable/DataTableDemo.dart';
 
 void main() {
   runApp(
@@ -12,72 +12,7 @@ class HomeApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: FirebaseMessagingDemo(),
+      home: DataTableDemo(),
     );
   }
 }
-
-/*
-void main() {
-  runApp(
-    ChangeNotifierProvider<AppStateNotifier>(
-      builder: (context) => AppStateNotifier(),
-      child: HomeApp(),
-    ),
-  );
-}
-
-
-class HomeApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Consumer<AppStateNotifier>(
-      builder: (context, appState, child) {
-        return MaterialApp(
-          title: 'Flutter Tutorials',
-          debugShowCheckedModeBanner: false,
-          theme: AppTheme.lightTheme,
-          darkTheme: AppTheme.darkTheme,
-          themeMode: appState.isDarkModeOn ? ThemeMode.dark : ThemeMode.light,
-          home: ThemeDemo(),
-        );
-      },
-    );
-  }
-}
-
-*/
-
-/*
-// Wrap main widget inside the StreamProvider
-class HomeApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return StreamProvider<ConnectivityResult>(
-      builder: (context) => ConnectivityService().connectionStatusController,
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Tutorials',
-        home: new ConnectivityDemo(),
-      ),
-    );
-  }
-}
-*/
-
-/*
-class HomeApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      //home: FirstScreen(),
-      routes: {
-        FirstScreen.routeId: (context) => FirstScreen(),
-        SecondScreen.routeId: (context) => SecondScreen(),
-      },
-      initialRoute: FirstScreen.routeId,
-    );
-  }
-}
-*/
